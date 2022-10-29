841b7a42a04b1ad0bb595de4ec1b48133fe2850e
dohyu <dohyunre702@gmail.com>
2022-10-29 23:15:14 +0900

    10/6 지정한 파일의 맨 앞 한 byte만을 읽어오는 method 리팩토링

--- src/main/java/week1/read/ReadAFile.java
+++ src/main/java/week1/read/ReadAFile.java
@@ -1,13 +1,21 @@
 package week1.read;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
-//지정한 파일의 맨 앞 한 byte를 읽어오는 method
+class FileExercise {
+    //지정한 파일의 맨 앞 한 byte를 읽어오는 method
+    public char readAChar(String filename) throws IOException {
+        FileReader fileReader = new FileReader(filename);
+        return (char) fileReader.read();
+    }
+}
+
 public class ReadAFile {
     public static void main(String[] args) throws IOException {
-        FileReader reader = new FileReader("./src/main/java/week1/read/8842height.txt");
-        char c = (char) reader.read();
+        FileExercise fileExercise = new FileExercise();
+        char c = fileExercise.readAChar("./src/main/java/week1/read/8842height.txt");
         System.out.println(c);
     }
 }
