d773c174f5e9df7e373eec48d9c5474767496343
dohyu <dohyunre702@gmail.com>
2022-11-03 10:27:20 +0900

    10/7 1. 한 라인씩 읽어오기 ver2

--- src/main/java/maxDataProject/PopulationStatistics.java
+++ src/main/java/maxDataProject/PopulationStatistics.java
@@ -1,21 +1,26 @@
 package maxDataProject;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class PopulationStatistics {
     // 한 라인씩 읽어오기
-    public String readByLine(String filename) throws IOException {
-        //FileReader fileReader = new FileReader("./src/main/java/week1/maxDataProject/2021populationdata.csv");
-        BufferedReader br = new BufferedReader(new FileReader("./src/main/java/week1/maxDataProject/2021populationdata.csv"));
-
-        String fileContents = "";
-        while ((fileContents = br.readLine()) != null) {
-            fileContents += br.readLine();
-        }
-        br.close();
-        return fileContents;
+    public String readByLine(String filename) {
+        String fileContents;
+        try ( //사용한 자원(BufferedREader) 자동 반납
+            BufferedReader br = Files.newBufferedReader( //BufferedReader 객체 생성
+                    Paths.get("./src/main/java/week1/maxDataProject/2021populationdata.csv"), StandardCharsets.UTF_8))
+        {
+            while ((fileContents = br.readLine()) != null) {
+                fileContents += br.readLine();
+                return fileContents;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }   return fileContents;
     }
 }
 
