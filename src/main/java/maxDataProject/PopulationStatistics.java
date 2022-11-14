483c67965164ff7517fe5b60872c0814a782d273
dohyu <dohyunre702@gmail.com>
2022-11-14 14:43:56 +0900

    10/7 실습 5. from_to 파일 완성

--- src/main/java/maxDataProject/PopulationStatistics.java
+++ src/main/java/maxDataProject/PopulationStatistics.java
@@ -1,12 +1,15 @@
 package maxDataProject;
 
 import java.io.*;
-import java.nio.Buffer;
 import java.util.ArrayList;
 import java.util.List;
 
 public class PopulationStatistics {
     Parser ps = new Parser();
+
+    public String fromToString(PopulationMove populationMove) {
+        return populationMove.getFromSido() + "," + populationMove.getToSido() +"\n";
+    }
 
     public List<PopulationMove> readByLine(String filename) throws IOException {
         List<PopulationMove> pml = new ArrayList<>();
@@ -23,6 +26,7 @@ public class PopulationStatistics {
         return pml;
     }
 
+    //파일에 저장
     public void write(List<String> strs, String filename) {
         File file = new File(filename);
 
@@ -38,18 +42,17 @@ public class PopulationStatistics {
     }
 
     public static void main(String[] args) throws IOException {
+        //데이터 수 10000개로 줄여서 진행
+        PopulationStatistics populationStatistics = new PopulationStatistics();
+
         String address = "C:/Users/dohyu/git/LikelionPractice/src/main/java/maxDataProject/2021populationdata.csv";
         //readLineContext new 할 때 Type(PopulationMove)-파싱로직(DoSomething의 구현체) 넣어서 생성.
-        PopulationStatistics populationStatistics = new PopulationStatistics();
         List<PopulationMove> pml = populationStatistics.readByLine(address);
-
-        //
-        // 파일 열 크기 출력 System.out.println(pml.size());
         List<String> strings = new ArrayList<>();
-        strings.add("11,11");
-        populationStatistics.write(strings, "./from_to.txt");
-
-
+        for (PopulationMove pm : pml) {
+            String fromTo = populationStatistics.fromToString(pm);
+            strings.add(fromTo);
+        }
+        populationStatistics.write(strings, "./from_to.txt"); //22초만에 10만 건 작성
     }
 }
-
